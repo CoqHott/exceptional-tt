e22f77c15ea97e8bb78084c828ed8d3a88f88d6f
Pierre-Marie Pédrot <pierre-marie.pedrot@inria.fr>
2016-11-29 19:47:28 +0100

    Adding a proof that linear operators commute with memorization for reader.

--- theories/reader/Alg.v
+++ theories/reader/Alg.v
@@ -58,8 +58,46 @@ Definition boolᶠ : TYPE := fun _ => bool.
 Definition trueᶠ : El boolᶠ := (fun _ => true).
 Definition falseᶠ : El boolᶠ := (fun _ => false).
 
-Definition bool_caseᶠ (P : TYPE) (Pt : El P) (Pf : El P) (b : El boolᶠ) : El P :=
+Definition bool_caseᶠ : El (Πᶠ (P : TYPE) (Pt : El P) (Pf : El P) (b : El boolᶠ), P) :=
+  λᶠ P Pt Pf b,
   @hbind bool _ b (fun b => if b then Pt else Pf).
 
-Check (eq_refl : (fun P Pt Pf => bool_caseᶠ P Pt Pf trueᶠ) = (fun P Pt Pf => Pt)).
-Check (eq_refl : (fun P Pt Pf => bool_caseᶠ P Pt Pf falseᶠ) = (fun P Pt Pf => Pf)).
+Check (eq_refl : (fun P Pt Pf => bool_caseᶠ · P · Pt · Pf · trueᶠ) = (fun P Pt Pf => Pt)).
+Check (eq_refl : (fun P Pt Pf => bool_caseᶠ · P · Pt · Pf · falseᶠ) = (fun P Pt Pf => Pf)).
+
+Definition θ_bool : El (Πᶠ (n : El boolᶠ), (boolᶠ →ᶠ Typeᶠ) →ᶠ Typeᶠ) :=
+  λᶠ n,
+  bool_caseᶠ · ((boolᶠ →ᶠ Typeᶠ) →ᶠ Typeᶠ) ·
+    (Lamᶠ (fun k => k · trueᶠ)) · (Lamᶠ (fun k => k · falseᶠ)) · n.
+
+Inductive box (A : TYPE) := Box : El A -> box A.
+
+Definition boxᶠ : El (Typeᶠ →ᶠ Typeᶠ) := λᶠ A, fun ω => box A.
+
+Definition Boxᶠ : El (Πᶠ (A : El Typeᶠ), A →ᶠ boxᶠ · A) :=
+  λᶠ A x, fun ω => Box A x.
+
+Definition box_caseᶠ : El (Πᶠ (A : El Typeᶠ) (P : El Typeᶠ),
+  (A →ᶠ P) →ᶠ boxᶠ · A →ᶠ P) :=
+  λᶠ A P f b, @hbind (box A) _ b
+    (fun b => match b return El P with Box _ x => f · x end).
+
+Definition equiv {A} (x y : El A) := forall ω, x ω = y ω.
+
+Notation "x ≡ y" := (equiv x y) (at level 70, no associativity).
+
+(** Linear definition taken from Paul Blain Levy *)
+Definition linear {A B} (f : El (A →ᶠ B)) : Prop :=
+  forall b : El (boxᶠ · A),
+    f · (box_caseᶠ · A · A · (λᶠ x, x) · b) ≡ box_caseᶠ · A · B · f · b.
+
+Lemma linear_bool_storage_compat : forall (P : El (boolᶠ →ᶠ Typeᶠ)),
+  linear P -> forall n, θ_bool · n · P ≡ P · n.
+Proof.
+intros P HP b ω.
+compute.
+pose (b₀ := fun ω => Box _ (fun _ => b ω)).
+specialize (HP b₀ ω).
+compute in HP; revert HP; clear b₀.
+destruct (b ω); intuition.
+Qed.
