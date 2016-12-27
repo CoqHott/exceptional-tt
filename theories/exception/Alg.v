883135e9669670ce10d2b7810ad67bbb654b44ce
Pierre-Marie Pédrot <pierre-marie.pedrot@inria.fr>
2016-12-27 20:14:56 +0100

    Reimplementing exceptions closer to the paper.

--- theories/exception/Alg.v
+++ theories/exception/Alg.v
@@ -1,106 +1,107 @@
 Set Universe Polymorphism.
 Set Primitive Projections.
 
-Axiom E : Type.
-
-Record prod A B := pair { fst : A; snd : B }.
-
-Arguments pair {_ _} _ _.
-Arguments fst {_ _} _.
-Arguments snd {_ _} _.
-
-Inductive M (A : Type) :=
-| Ok : A -> M A
-| Err : E -> M A.
-
-Definition map {A B} (f : A -> B) (l : M A) : M B :=
-match l with
-| Ok _ x => Ok _ (f x)
-| Err _ e => Err _ e
-end.
-
-Definition pointwise {A} (f : A -> Type) (l : M A) : Type :=
-match l with
-| Ok _ x => f x
-| Err _ _ => E
-end.
-
-Definition hlist (T : M Type) := pointwise (fun A => A) T.
-Definition is_alg (T : M Type) := pointwise (fun A => M A -> A) T.
-
-Record TYPE := {
-  wit : M Type;
-  alg : is_alg wit;
+Record sig A (P : A -> Type) := exist {
+  wit : A;
+  prf : P wit;
 }.
 
-Definition El (A : TYPE) : Type := hlist A.(wit).
+Arguments wit {_ _} _.
+Arguments prf {_ _} _.
 
-Fixpoint bind {A B} (l : M A) (f : A -> M B) : M B :=
-match l with
-| Ok _ x => f x
-| Err _ e => Err _ e
+Inductive sum A B :=
+| inl : A -> sum A B
+| inr : B -> sum A B.
+
+Arguments inl {_ _} _.
+Arguments inr {_ _} _.
+
+Axiom E : Type.
+
+Definition T A := sum A E.
+Definition ret {A} (x : A) : T A := inl x.
+Definition bnd {A B} (x : T A) (f : A -> T B) : T B :=
+match x with
+| inl x => f x
+| inr e => inr e
 end.
 
-Fixpoint pointwise_alg {T} (f : is_alg T) (l : M (hlist T)) : hlist T :=
-match T return is_alg T -> M (hlist T) -> hlist T with
-| Ok _ x => fun f l => f l
-| Err _ e => fun f l => e
-end f l.
+Definition TYPE := sig Type (fun A => T A -> A).
+Definition mkTYPE (A : Type) (μ : T A -> A) : TYPE := exist _ _ A μ.
+Definition mkFree (A : Type) : TYPE := mkTYPE (T A) (fun x => bnd x (fun x => x)).
 
-Definition Typeᶫ : TYPE.
-Proof.
-refine {|
-  wit := Ok _ TYPE;
-  alg := fun T => {| wit := bind T wit; alg := _ |};
-|}.
-unfold is_alg.
-destruct T as [A|e].
-+ apply A.(alg).
-+ exact e.
-Defined.
+Axiom Ω : E -> TYPE.
 
-Check Typeᶫ : El Typeᶫ.
-
-Definition Prodᶫ (A : TYPE) (B : El A -> TYPE) : TYPE := {|
-  wit := Ok _ (forall x : El A, El (B x));
-  alg := fun f x => pointwise_alg (B x).(alg) (map (fun f => f x) f)
-|}.
-
-Definition Lamᶫ {A B} (f : forall x : El A, El (B x)) : El (Prodᶫ A B) := f.
-Definition Appᶫ {A B} (f : El (Prodᶫ A B)) (x : El A) := f x.
-
-Definition boolᶫ : TYPE := {| wit := Ok _ (M bool); alg := fun b => bind b (fun b => b) |}.
-Definition trueᶫ : El boolᶫ := Ok _ true.
-Definition falseᶫ : El boolᶫ := Ok _ false.
-
-Fixpoint hbind {A B} (l : M A) (f : A -> El B) {struct l} : El B :=
-match l with
-| Ok _ x => f x
-| Err _ e => pointwise_alg B.(alg) (Err _ e)
+Definition El (A : T TYPE) : TYPE := match A with
+| inl X => X
+| inr e => Ω e
 end.
 
-Definition bool_caseᶫ (P : TYPE) (Pt : El P) (Pf : El P) (b : El boolᶫ) : El P :=
-  hbind b (fun b => if b then Pt else Pf).
+Notation "[| A |]" := (El A).(wit).
+
+Definition hbnd {A : Type} {B : T TYPE} (x : T A) (f : A -> [|B|]) : [|B|] :=
+match x with
+| inl x => f x
+| inr e => (El B).(prf) (inr e)
+end.
+
+Definition Typeᶫ : T TYPE := ret (mkFree TYPE).
+
+Check Typeᶫ : [| Typeᶫ |].
+
+(*
+Definition pbnd₀ (A : Type) (B : A -> T TYPE) (x : T A) (f : forall x, (El (B x)).(wit)) :
+  (El (hbnd A Typeᶫ x B)).(wit) :=
+match x return (El (hbnd A Typeᶫ x B)).(wit) with
+| inl x => f x
+| inr e => (Ω e).(prf) (inr e)
+end.
+*)
+
+Definition Prodᶫ (A : T TYPE) (B : [|A|] -> T TYPE) : T TYPE :=
+  ret (mkTYPE (forall x : [|A|], [|B x|]) (fun f x => hbnd f (fun f => f x))).
+
+Notation "A →ᶫ B" := (Prodᶫ A (fun _ => B)) (at level 99, right associativity, B at level 200).
+Notation "'Πᶫ'  x .. y , P" := (Prodᶫ _ (fun x => .. (Prodᶫ _ (fun y => P)) ..))
+  (at level 200, x binder, y binder, right associativity).
+
+Definition Lamᶫ {A B} (f : forall x : [|A|], [|B x|]) : [|Prodᶫ A B|] := f.
+Definition Appᶫ {A B} (f : [|Prodᶫ A B|]) (x : [|A|]) : [|B x|] := f x.
+
+Definition pbnd (A : Type) (R : T TYPE) (B : A -> [|R|] -> T TYPE) (x : T A)
+  (r : [|R|]) (f : forall x, [|B x r|]) :
+  [| @hbnd A (R →ᶫ Typeᶫ) x B r |] :=
+match x return [| @hbnd A (R →ᶫ Typeᶫ) x B r |] with
+| inl x => f x
+| inr e => (Ω e).(prf) (inr e)
+end.
+
+Definition boolᶫ : [|Typeᶫ|] := ret (mkFree bool).
+Definition trueᶫ : [|boolᶫ|] := ret true.
+Definition falseᶫ : [|boolᶫ|] := ret false.
+
+Definition bool_caseᶫ : [| Πᶫ (P : [|Typeᶫ|]) (Pt Pf : [|P|]), boolᶫ →ᶫ P |] :=
+  fun P Pt Pf b => hbnd b (fun b => if b then Pt else Pf).
 
 Check (eq_refl : (fun P Pt Pf => bool_caseᶫ P Pt Pf trueᶫ) = (fun P Pt Pf => Pt)).
 Check (eq_refl : (fun P Pt Pf => bool_caseᶫ P Pt Pf falseᶫ) = (fun P Pt Pf => Pf)).
 
-Definition bool_recᶫ (P : El boolᶫ -> TYPE)
-  (Pt : El (P trueᶫ)) (Pf : El (P falseᶫ)) (b : El boolᶫ) : El (bool_caseᶫ Typeᶫ (P trueᶫ) (P falseᶫ) b).
-Proof.
-induction b as [be|be]; cbn in *.
-+ destruct be; [exact Pt|exact Pf].
-+ exact be.
-Defined.
+Definition θ_bool : [| boolᶫ →ᶫ (boolᶫ →ᶫ Typeᶫ) →ᶫ Typeᶫ |] :=
+  fun b k =>
+    bool_caseᶫ ((boolᶫ →ᶫ Typeᶫ) →ᶫ Typeᶫ) (fun k => k trueᶫ) (fun k => k falseᶫ) b k.
+
+Definition bool_recᶫ : [|
+  Πᶫ (P : [|boolᶫ →ᶫ Typeᶫ|]) (Pt : [|P trueᶫ|]) (Pf : [|P falseᶫ|])
+  (b : [|boolᶫ|]), θ_bool b P
+|] := fun P Pt Pf b =>
+  pbnd bool (boolᶫ →ᶫ Typeᶫ) (fun b P => θ_bool (ret b) P) b P
+    (fun b => bool_rect (fun b => [| θ_bool (ret b) P |]) Pt Pf b).
 
 Check (eq_refl : (fun P Pt Pf => bool_recᶫ P Pt Pf trueᶫ) = (fun P Pt Pf => Pt)).
 Check (eq_refl : (fun P Pt Pf => bool_recᶫ P Pt Pf falseᶫ) = (fun P Pt Pf => Pf)).
 
-Definition Eᶫ : El Typeᶫ :=
-  {| wit := Ok _ E; alg := fun e => match e with Ok _ e => e | Err _ e => e end |}.
+Definition Eᶫ : [| Typeᶫ |] :=
+  ret (mkTYPE E (fun e => match e with inl e => e | inr e => e end)).
 
-Definition fail : El (Prodᶫ Eᶫ (fun _ => Prodᶫ Typeᶫ (fun A => A))) :=
-  fun e A => pointwise_alg A.(alg) (Err _ e).
-
-Goal El (Prodᶫ Typeᶫ (fun A => A)).
-compute.
+Definition fail : [| Πᶫ (e : [| Eᶫ |]) (A : [| Typeᶫ |]), A |] :=
+  fun e A => (El A).(prf) (inr e).
