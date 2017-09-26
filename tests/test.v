38419cb2e943e306e550545999f6ee12ff69f9eb
Pierre-Marie Pédrot <pierre-marie.pedrot@inria.fr>
2017-09-26 12:24:04 +0200

    Adding a few tests.

---
+++ tests/test.v
@@ -0,0 +1,39 @@
+Require Import Effects.Effects.
+
+Inductive foo (A : Type) (x : A) (y := x) (y : A) := Foo.
+
+Effect Translate foo.
+Effect Translate eq.
+
+Effect Translate eq_rect.
+
+Inductive bar : nat -> nat -> Type := Bar : bar 0 1.
+
+Effect Translate nat.
+Effect Translate bar.
+
+Effect Translate bool.
+Effect Translate list.
+Effect Translate nat_rect.
+Effect Translate False.
+
+Require Import Streams.
+
+Effect Translate Stream.
+Effect Translate tl.
+Effect Translate Nat.pred.
+Effect Translate Str_nth_tl.
+
+Effect Translate unit.
+
+Effect Translate sum.
+
+Effect Translate sum_rect.
+
+Definition test (A : Type) (f : A -> A) (b : bool) : A -> A :=
+match b with
+| true => f
+| false => fun x => x
+end.
+
+Effect Translate test.
