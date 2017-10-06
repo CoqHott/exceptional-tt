e57a3c2381e534ae6e03d290a17bd702cd1cf481
nicolas.tabareau <nicolas.tabareau@inria.fr>
2017-10-06 15:14:44 +0200

    more stuff in cast.v

--- tests/cast.v
+++ tests/cast.v
@@ -8,11 +8,48 @@ Class Decidable (A : Type) := dec : A + (not A).
 
 Arguments dec A {_}.
 
+
+
+Instance Decidable_eq_nat : forall (x y : nat), Decidable (eq x y).
+induction x.
+- destruct y.
+ + left ;reflexivity.
+ + right; intro H; inversion H.
+- induction y.
+  + right; intro H; inversion H.
+  + case (IHx y). intro H. left. exact (f_equal S H).
+    intro H; right. intro e. inversion e. apply (H H1).
+Defined.
+
+Class EqDecidable (A : Type) := { 
+  eq_dec : forall a b : A, Decidable (a = b) 
+                               }.
+
+Instance EqDecidable_nat : EqDecidable nat := 
+  { eq_dec := Decidable_eq_nat }.
+
+
 Effect Translate empty.
 Effect Translate not.
 Effect Translate sum.
 Effect Translate Decidable.
 Effect Translate dec.
+
+Effect Translate list.
+Effect Translate prod.
+Effect Translate eq.
+Effect Translate eq_ind.
+Effect Translate nat.
+Effect Translate nat_rect.
+Effect Translate Datatypes.length.
+Effect Translate True.
+Effect Translate False.
+Effect Translate False_ind. 
+Effect Translate False_rect. 
+Effect Translate f_equal.
+Effect Translate eq_sym.
+Effect Translate eq_ind_r.
+Effect Translate Decidable_eq_nat.
 
 Notation "x .1" := (projT1 x) (at level 3).
 Notation "x .2" := (projT2 x) (at level 3).
@@ -78,5 +115,33 @@ Definition cast (A:Type) (P : A -> Type)
   | inl p => (a ; p)
   | inr _ => raise "not castable"
   end.
-  
 
+Effect Translate cast using Exception.
+
+Definition list_to_pair {A} : list A -> A * A.
+Proof.
+  intro l. pose (cast (list A) (fun l => List.length l = 2) l). 
+  destruct s. destruct x.
+  inversion e.
+  destruct x. inversion e.
+  exact (a,a0).
+Defined.
+
+Effect Translate list_to_pair using Exception.
+
+Notation "[ ]" := nil (format "[ ]") : list_scope.
+Notation "[ x ]" := (cons x nil) : list_scope.
+Notation "[ x ; y ; .. ; z ]" := (cons x (cons y .. (cons z nil) ..)) : list_scope.
+Notation "[ x ; .. ; y ]" := (cons x .. (cons y nil) ..) (compat "8.4") : list_scope.
+
+Definition list_to_pair_prop A (x y : A) : list_to_pair [x ; y] = (x,y).
+Proof.
+  reflexivity.
+Defined.
+
+Effect Translate list_to_pair_prop using Exception.
+
+Definition list_to_pair_prop_fake A (x y : A) : list_to_pair [x ; y] = (x,y) :=
+  match (raise "Fake Proof") : empty with end. 
+
+Effect Translate list_to_pair_prop_fake using Exception.
