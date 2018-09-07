a5bd50f375f41eed846e88378d5662c005b58f42
tabareau <nicolas.tabareau@inria.fr>
2018-09-07 18:37:26 +0200

    rework the cast example in the new framework

--- tests/cast.v
+++ tests/cast.v
@@ -1,73 +1,13 @@
 Require Import Weakly.Effects.
 
-Inductive empty : Type := .
-
-Definition not (T:Type) : Type := T -> empty.
-
-Class Decidable (A : Type) := dec : A + (not A).
-
-Arguments dec A {_}.
-
-Instance Decidable_eq_nat : forall (x y : nat), Decidable (eq x y).
-induction x.
-- destruct y.
- + left ;reflexivity.
- + right; intro H; inversion H.
-- induction y.
-  + right; intro H; inversion H.
-  + case (IHx y). intro H. left. exact (f_equal S H).
-    intro H; right. intro e. inversion e. apply (H H1).
-Defined.
-
-Class EqDecidable (A : Type) := { 
-  eq_dec : forall a b : A, Decidable (a = b) 
-                               }.
-
-Instance EqDecidable_nat : EqDecidable nat := 
-  { eq_dec := Decidable_eq_nat }.
-
-
-Effect Translate empty.
-Effect Translate not.
-Effect Translate sum.
-Effect Translate Decidable.
-Effect Translate dec.
-Effect Translate list.
-Effect Translate prod.
-Effect Translate eq.
-Effect Translate eq_ind.
-Effect Translate nat.
-Effect Translate nat_rect.
-Effect Translate length.
-Effect Translate True.
 Effect Translate False.
-Effect Translate False_ind. 
-Effect Translate False_rect. 
-Effect Translate f_equal.
-Effect Translate eq_sym.
-Effect Translate eq_ind_r.
-Effect Translate Decidable_eq_nat.
 
-Notation "x .1" := (projT1 x) (at level 3).
-Notation "x .2" := (projT2 x) (at level 3).
-Notation " ( x ; p ) " := (existT _ x p).
-
-Effect Translate sigT.
-Effect Translate projT1.
-Effect Translate projT2.
-
-Require Import String. 
-
-(* Definition Exception : Type := string.  *)
-
-Effect Translate bool.  
-Effect Translate Ascii.ascii.  
-Effect Translate string.  
-Effect Translate string_rect.  
+Effect Translate False_ind.
+Effect Translate False_rect.
 
 Effect Definition Exception : Type.
 Proof.
-  exact (fun E : Type => TypeVal E E id).
+  exact (fun E : Type => TypeVal E E (@id E)).
 Defined. 
 
 Effect Definition raise : forall A, Exception -> A.
@@ -77,109 +17,130 @@ Defined.
 
 Arguments raise [A] _.
 
+(* Define specific eliminations: parametric for Prop, default raise for Type *)
 
-Effect Definition Try_bool : forall (P : bool -> Type), P true -> P false -> (forall e, P (raise e)) -> forall b, P b.
+Inductive sum (A B : Prop) : Type :=  inl : A -> sum A B | inr : B -> sum A B.
+
+Notation "A + B" := (sum A B) : type_scope.
+Arguments inl {_ _} _.
+Arguments inr {_ _} _.
+
+Effect Translate sum.
+Effect Translate sum_rect. 
+Effect Translate sum_rec. 
+
+(* basic inversion lemmas for nat *)
+
+Effect Translate not.
+Effect Translate nat.
+Effect Translate eq.
+
+Effect Translate eq_ind.
+Effect Translate eq_rect.
+Effect Translate eq_rec.
+
+
+Effect Translate nat_rect.
+Effect Translate nat_rec. 
+
+Effect Definition S_not0 : forall n, 0 <> S n.
 Proof.
-  intros Exc P Pt Pf Perr b. destruct b.
-  - exact Pt.
-  - exact Pf.
-  - exact (Perr e).
+  compute; intros. inversion H.
 Defined.
 
-Scheme listᵒ_ind := Induction for listᵒ Sort Prop.
-Scheme listᵒ_rect := Induction for listᵒ Sort Type.
-
-Effect Definition Try_list : forall A (P : list A -> Type), P nil ->  (forall (a : A) (l : list A), P l -> P (cons a l)) -> (forall e, P (raise e)) -> forall l, P l.
-Proof.
-  intros Exc A P Pnil Pcons Perr l. induction l.
-  - exact Pnil.
-  - apply Pcons. exact IHl. 
-  - exact (Perr e).
+Effect Definition inj_S : forall n m, S n = S m -> n = m. 
+intros E n m e. inversion e; econstructor.
 Defined.
 
+(* boilerplate for eq, can be removed if pattern mathcing on eq is correctly translated*)
 
+(* Decidable type class *)
 
-Definition lift_bool : boolᵒ string -> bool + string.
-  destruct 1. exact (inl true). exact (inl false).
-  exact (inr s).
-Defined.
-                
-Definition lift_ascii : asciiᵒ string -> Ascii.ascii + string.
-  destruct 1.
-  destruct (lift_bool b) as [b'| e]; [idtac | exact (inr e)].
-  destruct (lift_bool b0) as [b0'| e]; [idtac | exact (inr e)].
-  destruct (lift_bool b1) as [b1'| e]; [idtac | exact (inr e)].
-  destruct (lift_bool b2) as [b2'| e]; [idtac | exact (inr e)].
-  destruct (lift_bool b3) as [b3'| e]; [idtac | exact (inr e)].
-  destruct (lift_bool b4) as [b4'| e]; [idtac | exact (inr e)].
-  destruct (lift_bool b5) as [b5'| e]; [idtac | exact (inr e)].
-  destruct (lift_bool b6) as [b6'| e]; [idtac | exact (inr e)].
-  exact (inl (Ascii.Ascii b' b0' b1' b2' b3' b4' b5' b6')).
-  exact (inr s).
-Defined. 
+Class Decidable (A : Prop) : Type := dec : A + (not A).
   
-Fixpoint lift_Exception (s:stringᵒ string) : string.
-Proof.
-  destruct s.
-  - exact EmptyString.
-  - apply lift_ascii in a. destruct a.
-    + apply String. exact a. apply (lift_Exception s).
-    + exact s0.
-  - exact s.
+Arguments dec A {_}.
+
+Class EqDecidable (A : Type) :=
+  { eq_dec : forall a b : A, Decidable (a = b) }.
+
+Instance Decidable_eq_nat : forall (x y : nat), Decidable (x = y).
+induction x.
+- induction y.
+  + left. apply eq_refl.
+  + right. apply S_not0. 
+- induction y.
+  + right. intro H. symmetry in H. destruct (S_not0 _ H).
+  + induction (IHx y). left. f_equal; auto. 
+    right. intro e. apply inj_S in e. apply (b e).
 Defined.
 
-Effect Definition lift : string -> Exception using string. 
-intros E. exact lift_Exception.
+Instance EqDecidable_nat : EqDecidable nat := 
+  { eq_dec := Decidable_eq_nat }.
+
+Effect List Translate list prod Decidable dec length True
+       eq_sym eq_trans f_equal Decidable_eq_nat.
+
+Notation " ( x ; p ) " := (exist _ x p).
+
+Effect List Translate sig sig_rect sig_rec list_rect.
+
+Effect Definition cast_fail : Exception.
+Admitted. 
+
+Definition cast (A:Type) (P : A -> Prop)
+           (a:A) {Hdec : Decidable (P a)} : {a : A | P a}.
+  induction (dec (P a)) using sum_rect. 
+  - exact (a ; a0).
+  - exact (raise cast_fail).
 Defined. 
 
-Coercion lift : string >-> Exception.
+Effect Translate cast.
 
-Local Open Scope string_scope.
-
-Definition cast (A:Type) (P : A -> Type)
-  (a:A) {Hdec : Decidable (P a)} : {a : A & P a} :=
-  match dec (P a) with
-  | inl p => (a ; p)
-  | inr _ => raise "cast fail"
-  end.
-
-Effect Translate cast using string.
-
-Definition list2_to_pair {A} : {l : list A & List.length l = 2} -> A * A.
+Definition list2_to_pair {A} : {l : list A | length l = 2} -> A * A.
 Proof.
-  destruct 1. destruct x.
-  inversion e.
-  destruct x. inversion e.
-  exact (a,a0).
+  induction 1. induction x.
+  - induction (S_not0 _ p) using False_rect.
+  - induction x.
+    + apply inj_S in p. induction (S_not0 _ p) using False_rect.
+    + exact (a , a0).
 Defined.
+
+Effect Translate list2_to_pair.
 
 Definition list_to_pair {A} : list A -> A * A.
 Proof.
-  exact (fun l => list2_to_pair (cast (list A) (fun l => List.length l = 2) l)).  
+  exact (fun l => list2_to_pair (cast (list A) (fun l => length l = 2) l)).  
 Defined.
+
+Effect Translate list_to_pair.
 
 Notation "[ ]" := nil (format "[ ]") : list_scope.
 Notation "[ x ]" := (cons x nil) : list_scope.
 Notation "[ x ; y ; .. ; z ]" := (cons x (cons y .. (cons z nil) ..)) : list_scope.
 Notation "[ x ; .. ; y ]" := (cons x .. (cons y nil) ..) (compat "8.4") : list_scope.
 
-Definition list_to_pair_prop A (x y : A) : list_to_pair [x ; y] = (x,y).
+Definition list_to_pair_prop A (x : A) e : list_to_pair [x ; raise e] = (x, raise e).
 Proof.
   reflexivity.
 Defined.
 
-Effect Translate list2_to_pair using string.
-Effect Translate list_to_pair using string.
-Effect Translate list_to_pair_prop using string.
+Effect Translate list_to_pair_prop.
 
-Definition list_to_pair_prop_soundness A x y :
-  list_to_pair_propᵉ A x y = eq_reflᵉ  _ _ _ := eq_refl. 
+(* sanity check *)
 
-Definition list_to_pair_prop_fake A (x y : A) : list_to_pair [x ; y] = (x,y) :=
-  raise "Fake Proof". 
+Definition list_to_pair_prop_soundness E A x y :
+  list_to_pair_propᵉ E A x y = eq_reflᵉ  _ _ _ := eq_refl. 
 
-Effect Translate list_to_pair_prop_fake using string.
+(* this one should be forbidden, because of non cummulativity *)
 
-Fail Definition list_to_pair_prop_soundness A x y :
-  list_to_pair_prop_fakeᵉ A x y = eq_reflᵉ  _ _ _ := eq_refl. 
+Definition list_to_pair_prop_fake : forall A (x y : A) e,
+    list_to_pair (raise e) = (x,y).
+
+Fail Effect Translate list_to_pair_prop_fake.
+
+Definition list_to_pair_prop_fake2 : forall A (x y : A),
+    list_to_pair [x;y;y] = (x,y).
+
+Fail Effect Translate list_to_pair_prop_fake2.
+
 
